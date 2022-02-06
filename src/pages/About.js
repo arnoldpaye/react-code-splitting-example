8190833cd52a66d9ed532f9081f60e8f4640a19c
Arnold Paye <arnoldpayet@gmail.com>
2022-02-06 08:08:52 -0400

    add One, Two, and Three components to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { One } from './One';
+import { Two } from './Two';
+import { Three } from './Three';
 
 export const About = () => {
   return(
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <One />
+      <Two />
+      <Three />
+    </>
   )
 };
